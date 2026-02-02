31777890a556a58ed3a79396a9592b40940253dd
Shaikh AtifaTanveer <145202138+ShaikhAtifa@users.noreply.github.com>
2026-02-02 23:43:35 +0530

    Implement pattern printing in P1.java
    Fix variable names and add missing logic for printing rows and columns.

---
+++ P1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class P1{
+public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("");
+    int totalrows=sc.nextInt();
+    for(int i=1;i<=n;i++)
+      {
+        for(int j=1;j<=n;j++)
+          {
+            if(i==1 || i==totalrows || j==1 || j==totalcols)
+            {
+              System.out.print("*");
+            }
+            else
+            {
+               System.out.print("  ");
+            }
+          }
+       
+      }
+  }
+  
+}
